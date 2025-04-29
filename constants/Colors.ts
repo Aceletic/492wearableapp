0cd6badbbdcc35f0fca434d47046edaf5211a194
Mase <109102581+Aceletic@users.noreply.github.com>
2025-04-29 06:44:39 -0500

    Add files via upload

---
+++ constants/Colors.ts
@@ -0,0 +1,25 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+  },
+  primary100: "#06aed5", 
+  primary200: "#086788", 
+  primary300: "#fff1d0", 
+  primary400: "#dd1c1a", 
+  primary500: "#f0c808",
+  primary600: "#d45c5b"
+};
